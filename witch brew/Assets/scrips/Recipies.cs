79761ca30499f7f4fe19ed84fe7798b1cadb8003
saltywaffuls <tabanpl@gmail.com>
2021-09-29 22:24:32 -0500

    intial cmmit

---
+++ witch brew/Assets/scrips/Recipies.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipies : MonoBehaviour
+{
+    [SerializeField] int numItems;
+    [SerializeField] Item[] items;
+}
